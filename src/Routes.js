0a102460d8cdef0650ebaae858c75406ca4e2ef1
priyanshu4705 <priyanshusrivastava4705@gmail.com>
2020-08-18 11:14:24 +0530

    written blog

--- src/Routes.js
+++ src/Routes.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Home from './components/Home'
+import Blog from './components/Blog'
 import About from './components/About'
-import Contact from './components/Contact'
 import NotFound from './components/NotFound'
 
 function Routes() {
     return (
         <Switch>
             <Route exact path="/" component={Home} />
-            <Route exact path="/about" component={About} />
-            <Route exact path="/contact" component={Contact} />
+            <Route exact path="/Blog" component={Blog} />
+            <Route exact path="/About" component={About} />
             <Route component={NotFound} />
         </Switch>
     )

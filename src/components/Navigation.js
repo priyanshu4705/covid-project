9dfbcb4e468f5dfd97e8907309231a714d51ce52
priyanshu4705 <priyanshusrivastava4705@gmail.com>
2020-08-19 13:56:36 +0530

    Completed Navigation drawer with rough css

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,24 @@
 import React, { useState } from 'react'
-import { Box, AppBar, Toolbar, IconButton, Typography, SwipeableDrawer, List, ListItem } from '@material-ui/core'
-import { Menu, Home } from '@material-ui/icons'
+import {
+    Box,
+    AppBar,
+    Toolbar,
+    IconButton,
+    Typography,
+    Drawer,
+    List,
+    ListItem,
+    ListItemIcon,
+    ListItemText
+} from '@material-ui/core'
+import {
+    Menu,
+    Home,
+    Book,
+    People,
+    GitHub,
+    Storage
+} from '@material-ui/icons'
 import '../assets/css/Navigation.css'
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom'
@@ -11,35 +29,74 @@ function Navigation() {
     console.log(state);
     const useStyles = makeStyles({
         list: {
-            width: 250,
-            fontSize: "1rem",
-            fontFamily: "Audiowide",
+            width: 240,
+            height: "100vh",
             backgroundColor: "#1e1e30",
             color: "#007bff",
+            paddingTop: "3rem",
         },
-        
+        item: {
+            padding: "2rem 1rem",
+        },
+        link: {
+            textDecoration: "none",
+            color: "#007bff",
+        }
     });
 
     const classes = useStyles();
 
     return (
-        <Box component="nav">
-            <AppBar style={{ backgroundColor: "#1e1e30" }}>
-                <Toolbar>
-                    <IconButton onClick={() => setState(true)}><Menu fontSize="large" style={{ color: "#bdbdbd" }} /></IconButton>
-                    <SwipeableDrawer anchor="left" open={state} onClose={() => setState(false)} onOpen={() => setState(true)}>
-                        <div className={classes.list} role="presentation" onClick={() => setState(false)}>
-                            <List>
-                                <ListItem button key="home" component={Link} to="/">
-                                    <Home /> Home
-                                </ListItem>
-                            </List>
-                        </div>
-                    </SwipeableDrawer>
-                    <Typography variant="h5" style={{ paddingLeft: "1rem", fontFamily: "Audiowide", color: "#bdbdbd" }}>COVID-19 PROJECT</Typography>
-                </Toolbar>
-            </AppBar>
-        </Box>
+        <>
+            <Box component="nav">
+                <AppBar style={{ backgroundColor: "#1e1e30" }}>
+                    <Toolbar>
+                        <IconButton onClick={() => setState(true)}><Menu fontSize="large" style={{ color: "#bdbdbd" }} /></IconButton>
+                        <Drawer anchor="left" open={state} onClose={() => setState(false)} onOpen={() => setState(true)}>
+                            <div className={classes.list} role="presentation" onClick={() => setState(false)}>
+                                <List>
+                                    <ListItem button className={classes.item} component={Link} to="/" key="home">
+                                        <ListItemIcon style={{ fontSize: "1.125rem", color: "#007bff" }}><Home /></ListItemIcon>
+                                        <ListItemText><Typography style={{ fontFamily: "Audiowide", fontSize: "1.125rem" }}>
+                                            Home
+                                        </Typography></ListItemText>
+                                    </ListItem>
+                                    <ListItem button className={classes.item} component={Link} to="/blog" key="blog">
+                                        <ListItemIcon style={{ fontSize: "1.125rem", color: "#007bff" }}><Book /></ListItemIcon>
+                                        <ListItemText><Typography style={{ fontFamily: "Audiowide", fontSize: "1.125rem" }}>
+                                            Blog
+                                        </Typography></ListItemText>
+                                    </ListItem>
+                                    <ListItem button className={classes.item} component={Link} to="/about" key="about">
+                                        <ListItemIcon style={{ fontSize: "1.125rem", color: "#007bff" }}><People /></ListItemIcon>
+                                        <ListItemText><Typography style={{ fontFamily: "Audiowide", fontSize: "1.125rem" }}>
+                                            About us
+                                        </Typography></ListItemText>
+                                    </ListItem>
+                                    <a className={classes.link} href="https://github.com/priyanshu4705/covid-project" target="_blank" rel="noopener noreferrer">
+                                        <ListItem button className={classes.item} key="github">
+                                            <ListItemIcon style={{ fontSize: "1.125rem", color: "#007bff" }}><GitHub /></ListItemIcon>
+                                            <ListItemText><Typography style={{ fontFamily: "Audiowide", fontSize: "1.125rem" }}>
+                                                GitHub
+                                            </Typography></ListItemText>
+                                        </ListItem>
+                                    </a>
+                                    <a className={classes.link} href="https://disease.sh/v3/covid-19/countries" target="_blank" rel="noopener noreferrer">
+                                        <ListItem button className={classes.item} key="api">
+                                            <ListItemIcon style={{ fontSize: "1.125rem", color: "#007bff" }}><Storage /></ListItemIcon>
+                                            <ListItemText><Typography style={{ fontFamily: "Audiowide", fontSize: "1.125rem" }}>
+                                                API
+                                            </Typography></ListItemText>
+                                        </ListItem>
+                                    </a>
+                                </List>
+                            </div>
+                        </Drawer>
+                        <Typography variant="h5" style={{ paddingLeft: "1rem", fontFamily: "Audiowide", color: "#bdbdbd" }}>COVID-19 PROJECT</Typography>
+                    </Toolbar>
+                </AppBar>
+            </Box>
+        </>
     )
 }
 
